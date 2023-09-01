5778ada814d1cce928b6c4c8350e36e4fce1570f
Jason Hoang <jchnetworking@gmail.com>
2023-08-31 21:24:30 -0400

    added vlan_macs endpoint

--- aoscx_handler.py
+++ aoscx_handler.py
@@ -17,8 +17,26 @@ class AOSCX_HANLDER:
     def login(self, login_info:dict[str,str]) -> requests.Response:
         return self._post('login',login_info)
 
-    def get_vlans(self):
-        return self._get('system/vlans').json()
+    def get_ip_bindings(self) -> dict:
+        ip_bindings = self._get('system/ip_bindings')
+        data = self._check_response_status_code(ip_bindings)
+        return data
+
+    def get_vlan_macs(self, vlan:str) -> dict:
+        vlan_macs = self._get(f'system/vlans/{vlan}/macs')
+        data = self._check_response_status_code(vlan_macs)
+        return data
+
+    def get_vlans(self) -> dict:
+        vlans = self._get('system/vlans')
+        data = self._check_response_status_code(vlans)
+        return data
+
+    def _check_response_status_code(self, resp:requests.Response) -> dict:
+        if resp.status_code != 200:
+            raise ConnectionError('Failed to call the API endpoint.')
+        else:
+            return resp.json()
     
     def _post(self, uri:str, data:dict[str, str], headers:dict[str,str]={}) -> requests.Response:
         if headers != {}:
